2f61e55819d4a1aea94f3863a23b815ae56639ad
zainab alkamal <zainab.alkamal@gmail.com>
2024-08-28 16:31:33 +0300

    Progress on queries: XXX tests passing

--- queries.py
+++ queries.py
@@ -1,27 +1,59 @@
-# pylint: disable=missing-docstring, C0103
+import sqlite3
 
 def directors_count(db):
-    # return the number of directors contained in the database
-    pass  # YOUR CODE HERE
+    query = "SELECT CAST(COUNT(name) AS INTEGER) FROM directors"
+    db.execute(query)
+    result = db.fetchall()
+    return result[0][0]
+
 
 
 def directors_list(db):
-    # return the list of all the directors sorted in alphabetical order
-    pass  # YOUR CODE HERE
+    query = "SELECT name FROM directors ORDER BY name ASC;"
+    db.execute(query)
+    result = db.fetchall()
+    return [name[0] for name in result]
+
 
 
 def river_movies(db):
-    # return the list of all movies which contain the exact word "river"
-    # in their title, sorted in alphabetical order
-    pass  # YOUR CODE HERE
+    # Use word boundaries to ensure "river" is matched as a whole word, not as a substring
+    query = """
+        SELECT title
+        FROM movies
+        WHERE title LIKE '% River %'  -- 'River' in the middle of the title
+        OR title LIKE 'River %'       -- 'River' at the start of the title
+        OR title LIKE '% River'       -- 'River' at the end of the title
+        OR title = 'River'            -- The title is exactly 'River'
+        ORDER BY title ASC; """
+    db.execute(query)
+    result = db.fetchall()
+    return [title[0] for title in result]
+
+
 
 
 def directors_named_like_count(db, name):
-    # return the number of directors which contain a given word in their name
-    pass  # YOUR CODE HERE
+    query = "SELECT COUNT(*) FROM directors WHERE name LIKE ?;"
+    db.execute(query, ('%' + name + '%',))
+    result = db.fetchone()
+    return result[0]
+
 
 
 def movies_longer_than(db, min_length):
-    # return this list of all movies which are longer than a given duration,
-    # sorted in the alphabetical order
-    pass  # YOUR CODE HERE
+    query = "SELECT title FROM movies WHERE minutes > ? ORDER BY title ASC;"
+    db.execute(query, (min_length,))
+    result = db.fetchall()
+    return [title[0] for title in result]
+
+
+
+# Main part of the program
+def main():
+    conn = sqlite3.connect('data/movies.sqlite')
+    db = conn.cursor()
+
+    print("Number of directors:", directors_count(db))
+
+    conn.close()
